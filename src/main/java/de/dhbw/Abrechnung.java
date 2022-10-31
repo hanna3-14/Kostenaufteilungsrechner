095ae7320a2baf91946fd0fcbcd457190257bebd
hanna3-14 <114485436+hanna3-14@users.noreply.github.com>
2022-10-31 19:08:48 +0100

    Methoden zur Berechnung von Gesamtausgaben und Bilanzen implementiert

--- src/main/java/de/dhbw/Abrechnung.java
+++ src/main/java/de/dhbw/Abrechnung.java
@@ -1,6 +1,8 @@
 package de.dhbw;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.UUID;
 
 public class Abrechnung {
@@ -15,6 +17,10 @@ public class Abrechnung {
 		this.eventID = eventID;
 		this.gesamtausgaben = gesamtausgaben;
 		this.bilanzen = bilanzen;
+	}
+
+	public Abrechnung(UUID eventID) {
+		this(eventID, new Geldbetrag(0.0), new HashMap<>());
 	}
 
 	public UUID getAbrechnungsID() {
@@ -53,5 +59,47 @@ public class Abrechnung {
 				", gesamtausgaben=" + gesamtausgaben +
 				", bilanzen=" + bilanzen +
 				'}';
+	}
+
+	public void berechneGesamtausgaben(Event event) {
+		List<Ausgabe> ausgabenListe = event.getAusgabenListe();
+
+		// Berechnung der Gesamtausgaben
+		Geldbetrag gesamtausgaben = new Geldbetrag(0.0);
+		for (Ausgabe a : ausgabenListe) {
+			gesamtausgaben = new Geldbetrag(gesamtausgaben.getWert() + a.getGeldbetrag().getWert());
+		}
+		this.setGesamtausgaben(gesamtausgaben);
+	}
+
+	public void berechneBilanzen(Event event) {
+		List<Ausgabe> ausgabenListe = event.getAusgabenListe();
+
+		Geldbetrag startbetrag = new Geldbetrag(0.0);
+		LinkedHashMap<Integer, Geldbetrag> bilanzen = new LinkedHashMap<>();
+
+		// Alle Mitglieder, die an einer Ausgabe des Events beteiligt sind, werden mit einem Startbetrag von 0.00 Euro hinzugefügt
+		for (Ausgabe a : ausgabenListe) {
+			bilanzen.put(a.getBezahlerID(), startbetrag);
+		}
+		for (Ausgabe a : ausgabenListe) {
+			for (int i : a.getEmpfaengerIDs()) {
+				bilanzen.put(i, startbetrag);
+			}
+		}
+
+		// Berechnung der Bilanzen für die einzelnen Mitglieder
+		for (Ausgabe a : ausgabenListe) {
+			for (int i : bilanzen.keySet()) {
+				if (a.getBezahlerID() == i) {
+					bilanzen.computeIfPresent(i, (k, v) -> v.increaseBetrag(a.getGeldbetrag()));
+				}
+				Geldbetrag anteiligerBetrag = new Geldbetrag(a.getGeldbetrag().getWert() / a.getEmpfaengerIDs().size());
+				if (a.getEmpfaengerIDs().contains(i)) {
+					bilanzen.computeIfPresent(i, (k, v) -> v.decreaseBetrag(anteiligerBetrag));
+				}
+			}
+		}
+		this.setBilanzen(bilanzen);
 	}
 }
