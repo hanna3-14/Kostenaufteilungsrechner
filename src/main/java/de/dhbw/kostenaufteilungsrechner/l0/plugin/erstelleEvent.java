c70f1d86fcf613c51a9a865c3bb16c9abf1989d6
hanna3-14 <114485436+hanna3-14@users.noreply.github.com>
2023-01-14 17:07:06 +0100

    Parameter zu 'erstelle-event' hinzugefügt

--- src/main/java/de/dhbw/kostenaufteilungsrechner/l0/plugin/erstelleEvent.java
+++ src/main/java/de/dhbw/kostenaufteilungsrechner/l0/plugin/erstelleEvent.java
@@ -4,23 +4,27 @@ import de.dhbw.kostenaufteilungsrechner.l1.adapters.EventDBAdapter;
 import de.dhbw.kostenaufteilungsrechner.l3.domain.Event;
 import picocli.CommandLine;
 
-import java.util.Scanner;
-
 @CommandLine.Command(name = "erstelle-event", description = "erstellt ein neues Event")
 public class erstelleEvent implements Runnable {
+
+	@CommandLine.Option(names = {"-b", "--beschreibung"}, description = "Beschreibung des Events")
+	String beschreibung;
+
+	@CommandLine.Option(names = {"-g", "--gruppenname"}, description = "Filtert die Ausgabe nach dem Gruppenname")
+	String gruppenName;
 
 	@Override
 	public void run() {
 
 		EventDBAdapter eventDBAdapter = new EventDBAdapter();
 
-		Scanner scan = new Scanner(System.in);
+		if (beschreibung == null) {
+			beschreibung = System.console().readLine("Eventbeschreibung: ");
+		}
 
-		System.out.println("Eventbeschreibung:");
-		String beschreibung = scan.nextLine();
-
-		System.out.println("Gruppenname:");
-		String gruppenName = scan.next();
+		if (gruppenName == null) {
+			gruppenName = System.console().readLine("Gruppenname: ");
+		}
 
 		Event event = new Event(beschreibung, gruppenName);
 		System.out.println();
