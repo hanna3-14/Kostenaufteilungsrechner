5959756e9d4346b058fdf7e78d1f32393114f85f
hanna3-14 <114485436+hanna3-14@users.noreply.github.com>
2023-02-03 20:51:19 +0100

    User Dialog für 'erstelle-event' verschönert (inklusive Fehlerbehandlung)

--- src/main/java/de/dhbw/kostenaufteilungsrechner/l0/plugin/erstelleEvent.java
+++ src/main/java/de/dhbw/kostenaufteilungsrechner/l0/plugin/erstelleEvent.java
@@ -1,8 +1,13 @@
 package de.dhbw.kostenaufteilungsrechner.l0.plugin;
 
 import de.dhbw.kostenaufteilungsrechner.l1.adapters.EventDBAdapter;
+import de.dhbw.kostenaufteilungsrechner.l1.adapters.GruppeDBAdapter;
 import de.dhbw.kostenaufteilungsrechner.l3.domain.Event;
+import de.dhbw.kostenaufteilungsrechner.l3.domain.Gruppe;
 import picocli.CommandLine;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @CommandLine.Command(name = "erstelle-event", description = "erstellt ein neues Event")
 public class erstelleEvent implements Runnable {
@@ -17,12 +22,34 @@ public class erstelleEvent implements Runnable {
 	public void run() {
 
 		EventDBAdapter eventDBAdapter = new EventDBAdapter();
+		GruppeDBAdapter gruppeDBAdapter = new GruppeDBAdapter();
+
+		Iterable<Gruppe> gruppen = gruppeDBAdapter.findeAlleGruppen();
+		List<String> gruppenNamen = new ArrayList<>();
+
+		for (Gruppe g : gruppen) {
+			gruppenNamen.add(g.getGruppenName());
+		}
 
 		if (beschreibung == null) {
 			beschreibung = System.console().readLine("Eventbeschreibung: ");
 		}
 
 		if (gruppenName == null) {
+			System.out.println("Für welche Gruppe soll ein Event erstellt werden?");
+			for (String s : gruppenNamen) {
+				System.out.println("\t" + s);
+			}
+			gruppenName = System.console().readLine("Gruppenname: ");
+		}
+
+		while (!gruppenNamen.contains(gruppenName)) {
+			System.out.println("ungültige Gruppe");
+			System.out.println();
+			System.out.println("Für welche Gruppe soll ein Event erstellt werden?");
+			for (String s : gruppenNamen) {
+				System.out.println("\t" + s);
+			}
 			gruppenName = System.console().readLine("Gruppenname: ");
 		}
 
