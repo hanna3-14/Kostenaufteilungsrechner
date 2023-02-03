89199b058d8c7279469831259bb04b66dab311ce
hanna3-14 <114485436+hanna3-14@users.noreply.github.com>
2023-02-03 21:15:39 +0100

    User Dialog für 'aktualisiere-event' verschönert (inklusive Fehlerbehandlung)

--- src/main/java/de/dhbw/kostenaufteilungsrechner/l0/plugin/aktualisiereEvent.java
+++ src/main/java/de/dhbw/kostenaufteilungsrechner/l0/plugin/aktualisiereEvent.java
@@ -1,9 +1,13 @@
 package de.dhbw.kostenaufteilungsrechner.l0.plugin;
 
 import de.dhbw.kostenaufteilungsrechner.l1.adapters.EventDBAdapter;
+import de.dhbw.kostenaufteilungsrechner.l1.adapters.GruppeDBAdapter;
 import de.dhbw.kostenaufteilungsrechner.l3.domain.Event;
+import de.dhbw.kostenaufteilungsrechner.l3.domain.Gruppe;
 import picocli.CommandLine;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @CommandLine.Command(name = "aktualisiere-event", description = "aktualisiert die Beschreibung und den Gruppennamen eines Events")
@@ -22,6 +26,14 @@ public class aktualisiereEvent implements Runnable {
 	public void run() {
 
 		EventDBAdapter eventDBAdapter = new EventDBAdapter();
+		GruppeDBAdapter gruppeDBAdapter = new GruppeDBAdapter();
+
+		Iterable<Gruppe> gruppen = gruppeDBAdapter.findeAlleGruppen();
+		List<String> gruppenNamen = new ArrayList<>();
+
+		for (Gruppe g : gruppen) {
+			gruppenNamen.add(g.getGruppenName());
+		}
 
 		Event event = eventDBAdapter.findeEventÜberID(eventID).orElse(null);
 
@@ -33,9 +45,21 @@ public class aktualisiereEvent implements Runnable {
 		if (beschreibung != null) {
 			event.setBeschreibung(beschreibung);
 		}
-		if (gruppenName != null) {
+
+		if (gruppenName != null && gruppenNamen.contains(gruppenName)) {
 			event.setGruppenName(gruppenName);
+			System.out.println("Für Event " + eventID + " wurde der Gruppenname aktualisiert auf: " + gruppenName);
 		}
+
+		if (gruppenName != null && !gruppenNamen.contains(gruppenName)) {
+			System.out.println("ungültiger Gruppenname");
+			System.out.println();
+			System.out.println("Verfügbare Gruppen:");
+			for (String s : gruppenNamen) {
+				System.out.println("\t" + s);
+			}
+		}
+
 		eventDBAdapter.aktualisiereEvent(event);
 	}
 }
